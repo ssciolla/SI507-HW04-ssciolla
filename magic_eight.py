223a4fa9700f56b59a08f08b876639a8672dfef9
ssciolla <samgsciolla@gmail.com>
2018-01-29 14:29:55 -0500

    Resetting master

--- magic_eight.py
+++ magic_eight.py
@@ -1,25 +1,4 @@
-import random
-
-def pick_answer():
-    list1 = ["It is certain", "It is decidedly so",
-    "Without a doubt", "Yes definitely", "You may rely on it",
-    "As I see it, yes", "Most likely", "Outlook good", "Yes",
-    "Signs point to yes", "Reply hazy try again", "Ask again later",
-    "Better not tell you now", "Cannot predict now",
-    "Concentrate and ask again", "Don't count on it", "My reply is no",
-    "My sources say no", "Outlook not so good", "Very doubtful"]
-    return(random.choice(list1))
 
 def request_question():
     question = input("What is your question? ")
-    return question
-
-in_progress = True
-while in_progress == True:
-    question = request_question()
-    if question == "quit":
-        in_progress = False
-    elif question[-1] != "?":
-        print("I'm sorry, I can only answer questions.")
-    else:
-        print(pick_answer())
+    
