c09ae6e5fda6ce5c271ffccddd3cd8ec58432574
ssciolla <samgsciolla@gmail.com>
2018-01-29 14:02:18 -0500

    Adding ability to check if question or quit

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,12 @@
 
 def request_question():
     question = input("What is your question? ")
+    return question
+
+in_progress = True
+while in_progress == True:
+    question = request_question()
+    if question == "quit":
+        in_progress = False
+    elif question[-1] != "?":
+        print("I'm sorry, I can only answer questions.)
